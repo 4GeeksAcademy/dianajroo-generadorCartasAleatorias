a0ac33cf212809dd147c62bc01baad21e531715f
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-02-20 20:45:45 +0000

    carta generador casicasi

--- src/app.js
+++ src/app.js
@@ -5,4 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {};
+window.onload = () => {
+  document.querySelector(".carta").classList.add("heart");
+  setTimeout(() => {
+    document.querySelector(".carta").classList.remove("heart");
+    document.querySelector(".carta").classList.add("spade");
+  }, 1000);
+};
