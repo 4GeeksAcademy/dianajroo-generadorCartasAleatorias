e740cef3b224699026210aed92b5cffe8d81ae3e
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-02-22 00:04:16 +0000

    generadorcartacasi

--- src/app.js
+++ src/app.js
@@ -5,10 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = () => {
-  document.querySelector(".carta").classList.add("heart");
-  setTimeout(() => {
-    document.querySelector(".carta").classList.remove("heart");
-    document.querySelector(".carta").classList.add("spade");
-  }, 1000);
-};
+window.onload =   function generarCarta() {
+  let palos = ['♦', '♠', '♣', '♥'];
+  let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+  let palo = palos[getRandomInt(0, palos.length - 1)];
+  let valor = valores[getRandomInt(0, valores.length - 1)];
+
+  return { valor: valor, palo: palo };
+}
+
+function mostrarCarta() {
+  let carta = generarCarta();
+  document.querySelector('.numero').textContent = carta.valor;
+  document.querySelector('.simboloSup').textContent = carta.palo;
+  document.querySelector('.simboloInfe').textContent = carta.palo;
+}
+
+mostrarCarta();
+});
+
