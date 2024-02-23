a723e4b654760bf1c506093aa2f606a854c1f5d0
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-02-23 22:28:49 +0000

    cartasRandomLista

--- src/app.js
+++ src/app.js
@@ -5,23 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload =   function generarCarta() {
-  let palos = ['♦', '♠', '♣', '♥'];
-  let valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+window.onload = () => {
+  function generarCarta() {
+    let palos = ["♦", "♠", "♣", "♥"];
+    let valores = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K",
+      "A"
+    ];
 
-  let palo = palos[getRandomInt(0, palos.length - 1)];
-  let valor = valores[getRandomInt(0, valores.length - 1)];
+    let palo = palos[Math.floor(Math.random() * palos.length)];
+    let valor = valores[Math.floor(Math.random() * valores.length)];
+    return { valor: valor, palo: palo };
+  }
 
-  return { valor: valor, palo: palo };
-}
+  function mostrarCarta() {
+    let carta = generarCarta();
+    document.querySelector(".numero").textContent = carta.valor;
+    let pintasup = document.querySelector(".simboloSup");
+    pintasup.textContent = carta.palo;
 
-function mostrarCarta() {
-  let carta = generarCarta();
-  document.querySelector('.numero').textContent = carta.valor;
-  document.querySelector('.simboloSup').textContent = carta.palo;
-  document.querySelector('.simboloInfe').textContent = carta.palo;
-}
+    let pintaInfe = document.querySelector(".simboloInfe");
+    pintaInfe.textContent = carta.palo;
+    if (carta.palo == "♥" || carta.palo == "♦") {
+      pintaInfe.style.color = "red";
+      pintasup.style.color = "red";
+    }
+  }
 
-mostrarCarta();
-});
-
+  mostrarCarta();
+};
